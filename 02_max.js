10fe6a8bdc79cfb32f10b3e56f297b5fbecfdcd2
Max Ray-Keil <maxray-keil@seattleacademy.org>
2016-02-09 14:57:36 -0800

    add 02_max.js

--- 02_max.js
+++ 02_max.js
@@ -1,4 +1,16 @@
-console.log(process.argv);
-var pin = process.argv[2];
-var status = process.argv[3];
-console.log("pin number" + pin + " turn" + status)
+var pin = process.argv[2]*1;
+console.log(pin);
+var wpi = require('wiring-pi');
+
+//console.log('wpi',wpi);
+wpi.setup('gpio');
+
+wpi.pinMode(pin,wpi.OUTPUT);
+
+var value = 1;
+
+function togleBulb(){
+	wpi.digitalWrite(pin, value);
+	value =+!value;
+}
+setInterval(toggleBulb, 500);
